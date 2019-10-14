eabf727dc0109f3e9084cb55c06a5f31e7e7d3e1
Alkaff Khan Ayup Khan <alkaffkhan@FWS-CHE-LT-2785.local>
2019-10-14 16:38:56 +0530

    Removed the redudant and applied before action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :destroy, :show, :update]
 
   def index
     @articles = Article.all
@@ -19,11 +20,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-  @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.new(article_params)
     if@article.update(article_params)
       flash[:notice] = "Article updated successfully"
       redirect_to article_path(@article)
@@ -33,17 +32,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article deleted successfully"
     redirect_to articles_path
   end
 
   def show
-      @article = Article.find(params[:id])
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
   params.require(:article).permit(:title, :description)
   end
